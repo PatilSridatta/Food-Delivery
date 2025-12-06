2986fd98ee4cb5e5c652f29607d9eb64239d30c2
PatilSridatta <patilsridatta@gmail.com>
2025-12-07 01:29:32 +0530

    Basic level template info

--- App.js
+++ App.js
@@ -1,6 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Hello World!!");
+
+
+/**
+ * Header
+ * Logo
+ * Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *      - RestaurantCard (many)
+ * Footer
+ *  - Links
+ *  - Copyright
+ *  - Contact Info
+ *  - Address
+ */
+
+// JSX (transpiled before it reaches the JS) - parcel -babel
+
+// JSX => Bebel transpiles it to React.createElement => ReactElement => JS Object => HTMLElement(render)
+
+const Title = () => {
+  return <h1 id="title">Hello from Title Component</h1>
+};
+
+
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <h1 id="heading2">Hello from Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Food Delivery app
+
+# JSX is not HTML/XML inside JS. It is HTML/XML like syntax
