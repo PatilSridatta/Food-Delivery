7542112c823f300c066fd6961c8e067b7c0aac3c
PatilSridatta <patilsridatta@gmail.com>
2026-07-03 00:38:21 +0530

    made website dynamic

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,42 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+  const data = await fetch(
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  );
+
+  const json = await data.json();
+
+  const restaurants =
+    json?.data?.cards
+      ?.find(
+        (card) =>
+          card?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      )
+      ?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+  console.log(restaurants);
+
+  setListOfRestaurants(restaurants || []);
+};
 
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
-          
           onClick={() => {
-            const filteredResList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+            const filteredResList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4,
+            );
             setListOfRestaurants(filteredResList);
           }}
         >
